d2238684ead311798ed62e34a69fc5c5fb9f8a5c
Gafisk <galihrestubaihq@gmail.com>
2025-03-22 08:30:16 +0700

    Fix Bug Delete Divisi

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,8 @@ Route::get('/administrator/tambah-divisi', [adminDivisiController::class, 'creat
 Route::post('/administrator/store-divisi}', [adminDivisiController::class, 'store'])->name('admin.store-divisi');
 Route::get('/administrator/edit-divisi/{id}', [adminDivisiController::class, 'edit'])->name('admin.edit-divisi');
 Route::put('/administrator/update-divisi/{id}', [adminDivisiController::class, 'update'])->name('admin.update-divisi');
-Route::delete('/administrator/delete-divisi/{id}', [adminDivisiController::class, 'destroy'])->name('admin.delete-divisi');
+Route::delete('/administrator/delete-divisi/{id}', [AdminDivisiController::class, 'destroy'])->name('admin.delete-divisi');
+
 
 // Kelola Berita
 Route::get('/administrator/daftar-berita', [adminBeritaController::class, 'index'])->name('admin.daftar-berita');
@@ -36,6 +37,6 @@ Route::get('/administrator/tambah-berita', [adminBeritaController::class, 'creat
 Route::post('/administrator/store-berita', [adminBeritaController::class, 'store'])->name('admin.store-berita');
 Route::get('/administrator/edit-berita/{id}', [adminBeritaController::class, 'edit'])->name('admin.edit-berita');
 Route::put('/administrator/update-berita/{id}', [adminBeritaController::class, 'update'])->name('admin.update-berita');
-Route::delete('/administrator/delete-divisi/{id}', [adminBeritaController::class, 'destroy'])->name('admin.delete-berita');
+Route::delete('/administrator/delete-berita/{id}', [adminBeritaController::class, 'destroy'])->name('admin.delete-berita');
 Route::put('/administrator/publish-berita/{id}', [adminBeritaController::class, 'publish'])->name('admin.publish-berita');
 Route::put('/administrator/draft-berita/{id}', [adminBeritaController::class, 'draft'])->name('admin.draft-berita');
