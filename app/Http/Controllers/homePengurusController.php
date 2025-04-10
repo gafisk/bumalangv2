e7900deb9b959b5c60aeaf0c8cf84df8cbf66fb6
Gafisk <galihrestubaihq@gmail.com>
2025-04-10 09:50:25 +0700

    Update View Home Pengurus

--- app/Http/Controllers/homePengurusController.php
+++ app/Http/Controllers/homePengurusController.php
@@ -19,7 +19,7 @@ class homePengurusController extends Controller
             return $item->divisi->nama_divisi;
         });
         // dd($datas);
-        return view('home.pengurus.profil-pengurus', compact('datas', 'data_organisasi'));
+        return view('home.profil.profil-pengurus', compact('datas', 'data_organisasi'));
     }
 
     /**
